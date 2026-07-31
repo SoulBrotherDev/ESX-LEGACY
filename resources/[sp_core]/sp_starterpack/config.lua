267814644f438f49b74249c2ff64c69826b97ad9
Soul Project <99090529+SoulBrotherDev@users.noreply.github.com>
2026-07-31 21:36:01 +0100

    Configure starter pack

---
+++ resources/[sp_core]/sp_starterpack/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.Enabled = true
+Config.Items = {
+    { name = 'phone', count = 1 },
+    { name = 'radio', count = 1 },
+    { name = 'bread', count = 2 },
+    { name = 'water', count = 2 }
+}
+
+Config.Notification = 'Recebeste o kit inicial da cidade.'
